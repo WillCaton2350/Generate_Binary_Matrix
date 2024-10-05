b01ae9bf8510d0b46f59d6b0d0bac2ca5a05b783
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-10-05 15:57:15 -0400

    Update SingletonBinaryMatrix.py

--- binarytree_content/SingletonBinaryMatrix.py
+++ binarytree_content/SingletonBinaryMatrix.py
@@ -1,19 +1,23 @@
 
 class GenerateMatrix:
     __instance = None
-    global nums, result
     nums = [114,112,109,118]
     result = []
 
     def __new__(cls,*args,**kwargs):
         if cls.__instance is None:
             cls.__instance = super(GenerateMatrix,cls).__new__(cls)
-            for i in nums:
+            cls.main()
+        return cls.__instance
+    
+    @classmethod
+    def main(cls):
+        if not cls.result:
+            for i in cls.nums:
                 val = bin(i)[2:-3] 
                 data = [int(i) for i in val]
-                result.append(data)
-            return result 
-        return cls.__instance
+                cls.result.append(data)
+
 
 if __name__ == "__main__":
     GenerateMatrix()
