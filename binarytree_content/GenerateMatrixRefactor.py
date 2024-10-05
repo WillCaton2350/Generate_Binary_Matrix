9869f0eb2ac710c9b7ef625633833102bb8d003a
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-10-05 15:26:48 -0400

    Update GenerateMatrixRefactor.py

--- binarytree_content/GenerateMatrixRefactor.py
+++ binarytree_content/GenerateMatrixRefactor.py
@@ -7,7 +7,10 @@ class GenerateMatrix:
     def binary_division(self):
         for i in nums:
             val = bin(i)[2:-3] 
-            data = [int(i) for i in val]
+            # slicing starting from the 0 and 1 index removes the standard 0b that identifes the binary representation of an integer. (eg: 0b1101)
+            # the bin() function handles the conversion from int to binary using binary division.
+            data = [int(i) for i in val] 
+            # The bin() function outputs the binary representation as a string so we need to type cast it using int() 
             result.append(data)
         return result 
 
